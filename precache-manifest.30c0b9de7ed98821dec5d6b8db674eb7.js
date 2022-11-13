51d03bd13bd7b968ac9d3b00209ddfc8d6429df8
Nadya Baranova <12nadya11@gmail.com>
2022-11-13 19:41:32 +0300

    Updates

---
+++ precache-manifest.30c0b9de7ed98821dec5d6b8db674eb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5123bf128789639dec61e154f61d2c25",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "a027173a430e40ea6620",
+    "url": "/simple-react-homeworks/static/css/main.45578e8b.chunk.css"
+  },
+  {
+    "revision": "7905d274468235d05e26",
+    "url": "/simple-react-homeworks/static/js/2.d7c8148f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.d7c8148f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a027173a430e40ea6620",
+    "url": "/simple-react-homeworks/static/js/main.de07d685.chunk.js"
+  },
+  {
+    "revision": "82c3ed739719c6917bd6",
+    "url": "/simple-react-homeworks/static/js/runtime-main.310d1191.js"
+  }
+]);
