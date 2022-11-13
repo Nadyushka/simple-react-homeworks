9ac6670206c16ec0decdde1b59eb82767c64c391
Nadya Baranova <12nadya11@gmail.com>
2022-11-13 22:35:20 +0300

    add gh-pages

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <div className={s.message}>
+            <img src={props.avatar} alt='no photo' className={s.avatarImg}/>
+            <div className={s.textInfoAndTime}>
+                <div className={s.textInfo}>
+                    <div className={s.textInfoTitle}>{props.name}</div>
+                    <div className={s.textInfoMessage}>{props.message}</div>
+                </div>
+                <div className={s.textInfoTime}>{props.time}</div>
+            </div>
 
         </div>
     )
