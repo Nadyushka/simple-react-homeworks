afb33de82cd88edfc4d3fe97ef8f52d8597134fa
Nadya Baranova <12nadya11@gmail.com>
2022-11-22 20:36:29 +0300

    correct mistakes

--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useState, ChangeEvent} from 'react'
+import React, {useState, ChangeEvent, FocusEvent} from 'react'
 import Greeting from './Greeting'
 import {UserType} from "./HW3";
 
@@ -14,13 +14,20 @@ type GreetingContainerPropsType = {
 // уровень локальной логики
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
     const [name, setName] = useState<string>('') // need to fix any
-    const [error, setError] = useState<string>('') // need to fix any
+    const [error, setError] = useState<string>('Ошибка! Введите имя') // need to fix any
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName(e.currentTarget.value) // need to fix
+        setError('')
     };
+
+
     const addUser = () => {
-        alert(`Hello  ${name}!`) // need to fix
+        // alert(`Hello  ${name}!`) // need to fix
+        addUserCallback(name)
+        setError('Ошибка! Введите имя')
+        setName('')
+
     }
 
     const totalUsers = users.length // need to fix
@@ -32,6 +39,7 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUser
             addUser={addUser}
             error={error}
             totalUsers={totalUsers}
+
         />
     )
 }
